4582ea2a7b10bf59abe74ed35ba3ac34084b6f0d
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-12-24 10:59:51 +0000

    update hotel wrapper

--- wrapper/hotel/run.sh
+++ wrapper/hotel/run.sh
@@ -17,16 +17,8 @@ output_dir="$6/out-$API.csv"
 address="192.168.1.100"
 
 if [ "$protocol" == "grpc" ]; then
-    if [ "$API" = "search-hotel" ]; then
-        args="lat=37.7867,lon=-122.4112,InDate=2024-08-15,OutDate=2024-08-17"
-        proto="protobuf.RajomonClient/SearchHotels"
-        url="$address:3000"
-    elif [ "$API" = "reserve-hotel" ]; then
-        echo "API 'reserve-hotel' is not supported"
-        exit 1
-    fi
-    ./rwg/rwg run -u $url -p $proto -d exp -D 2,$DURATION -r $BASE,$RATE -w 40000 -o $output_dir --args $args
-    exit "$?"
+    echo "GRPC is not supported"
+    exit 1
 else
     if [ "$API" = "search-hotel" ]; then
         url="http://$address:3000/hotels?lat=37.7867&lon=-122.4112&inDate=2024-08-15&outDate=2024-08-17"
@@ -36,6 +28,6 @@ else
         echo "Unknown hotel API: $API"
         exit 1
     fi
-    ./rwg/rwg run --url $url -d exp -D 2,$DURATION -r $BASE,$RATE -w 3000 -o $output_dir -t 15
+    ./rwg/rwg run --url $url -d exp -D 2,$DURATION -r $BASE,$RATE -w 5000 -o $output_dir -t 15
     exit "$?"
 fi
